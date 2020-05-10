b652a1979cc3f55c8ceb60f76efca8a2c6d207cd
wattenre <wattenre@gmail.com>
2020-05-10 14:08:01 +0200

    finished comments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate clap;
 extern crate image;
 
+use std::ffi::OsStr;
 use std::path::Path;
 
-use thumbnailer::{GenericThumbnail, Thumbnail};
+use thumbnailer::{GenericThumbnail, Target, Thumbnail};
+use thumbnailer::target::TargetFormat;
 
 use crate::cli::{get_matches, NAME_FILE_IN, NAME_FILE_OUT, read_commands};
 use crate::commands::CommandTrait;
@@ -11,18 +13,19 @@ use crate::commands::CommandTrait;
 pub mod cli;
 pub mod commands;
 
+/// Representation of the command-list as a struct
 pub struct Commands {
+    /// Contains the implementors of `CommandTrait` to apply a list of operations, which are provided by `thumbnailer`, on the supplied image(s)
     commands: Vec<Box<dyn CommandTrait>>,
 }
 
 //  .\target\debug\thumbnailer_cli.exe -h
-//  v
-//  .\target\debug\thumbnailer_cli.exe in.png out.png --flip_vertical  --brighten 15 --crop_ratio 4 3 --blur 34.151545 --unsharpen 56 14 --resize 89 4 true
-//
-//  .\target\debug\thumbnailer_cli.exe in.png out.png -c settings.conf -mmm watermark icon.png colored
-//  .\target\debug\thumbnailer_cli.exe in.png out.png -c settings.conf -mmm --blur 6 --brighten 15
-//  .\target\debug\thumbnailer_cli.exe in.png out.png --blur 6 --brighten 15
+//  .\target\debug\thumbnailer_cli.exe C:\Users\p372094\IdeaProjects\thumbnailer_cli\img\test.JPG C:\Users\p372094\IdeaProjects\thumbnailer_cli\img\result.png --blur 6 --brighten 15
 
+/// Main logic of the thumbnailer command line interface (`thumbnailer_cli`)
+///
+/// Run `thumbnailer_cli.exe -h` to view the help and learn about its functionality.
+/// Run `thumbnailer_cli.exe -V` to print the version information.
 fn main() {
     let matches = get_matches();
 
@@ -31,18 +34,23 @@ fn main() {
     let cmd_list = read_commands(matches);
 
     println!("Input file: {}", file_in);
-    let path_buf = Path::new(&file_in).to_path_buf();
-    let thumbnail = Thumbnail::load(path_buf);
-    let mut image: &mut dyn GenericThumbnail = &mut thumbnail.unwrap();
+    let mut image = Thumbnail::load(Path::new(&file_in).to_path_buf())
+        .unwrap_or_else(|_| panic!("‼→ ERROR in {}: failed to load the image with the supplied path ←‼", NAME_FILE_IN));
 
     for i in 0..cmd_list.commands.len() {
         println!("{}", cmd_list.commands.get(i).unwrap().print());
-        image = cmd_list.commands.get(i).unwrap().execute(image);
+        cmd_list.commands.get(i).unwrap().execute(&mut image);
     }
 
-
     println!("Output file: {}", file_out);
-
-    // TODO more program logic goes here...
+    let path = Path::new(&file_out);
+    let format = match path.extension().and_then(OsStr::to_str) {
+        Some("png") => TargetFormat::Png,
+        Some("tiff") => TargetFormat::Tiff,
+        Some("bmp") => TargetFormat::Bmp,
+        Some("gif") => TargetFormat::Gif,
+        _ => TargetFormat::Jpeg,
+    };
+    let mut target = Target::new(format, Path::new(&file_out).to_path_buf());
+    image.apply_store(&mut target);
 }
-
